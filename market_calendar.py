b138330cc279838cda687c7c35ad9d9517426b03
Nirmalk903 <nirmal.srivastava@gmail.com>
2025-05-01 11:25:19 +0530

    market calendar functions updated

--- market_calendar.py
+++ market_calendar.py
@@ -5,6 +5,10 @@ import pendulum as pm
 import json
 import yfinance as yf
 
+
+# The below function fetches the global economic calendar using the investpy library.
+# It filters the data for high-importance events and specific currencies (USD, GBP, EUR, INR, JPY, CNH).
+
 def economic_calendar():
     from_date=pm.now().subtract(days=1).strftime('%d/%m/%Y')
     to_date=pm.now().add(days=3).strftime('%d/%m/%Y')
@@ -20,12 +24,11 @@ def economic_calendar():
     df.columns = [col.title() for col in df.columns]
     df.style.format({'Date':'{:%b %Y}'})
     df.set_index('Date')
-        
-        
+    
     return df
 
 
-
+# The below function fetches the earnings calendar for a list of stock tickers using the yfinance library.
 
 
 def stock_earnings_calendar(tickers):
@@ -63,5 +66,51 @@ def stock_earnings_calendar(tickers):
     df = pd.concat(ls, ignore_index=True)
     df.set_index('Ticker', inplace=True)
     df.sort_values(by='Date', inplace=True)
+    
+    return df
+
+
+
+# The below code fetches the latest news articles related to Nifty50 stocks using the NewsAPI.
+# It requires an API key from NewsAPI or a similar service. The code fetches the latest news articles and prints the title, source, publication date, and URL of each article.
+
+
+import requests
+import pandas as pd
+
+def fetch_nifty50_news():
+    # Define the query parameters
+    api_key = "1b8de5003ddd4249bb3173cc8413a5dc"
+    from_date = pd.to_datetime(pm.now().subtract(days=1)) # .strftime('%d/%m/%Y')
+    to_date = pd.to_datetime(pm.now())
+    url = "https://newsapi.org/v2/everything"
+    query = "Nifty50 stocks OR Indian stock market OR Nifty50"
+    params = {
+        "q": query,
+        "language": "en",
+        "sortBy": "publishedAt",
+        "apiKey": api_key
+    }
+
+    # Make the API request
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        news_data = response.json()
+        articles = news_data.get("articles", [])
+        if articles:
+            
+            df = pd.DataFrame(articles)
+            df = df[['title', 'publishedAt', 'source']]
+            df['source'] = df['source'].apply(lambda x: x['name'])
+            df['title'] = df['title'].str.replace('Stock market update:', '', regex=False).str.strip()
+            df['title'] = df['title'].str.ljust(50)
+            df['publishedAt'] = pd.to_datetime(df['publishedAt'], errors='coerce')
+            df = df[0:10]  # Fetch top 5 articles
+            
+        else:
+            print("No news articles found.")
+    else:
+        print(f"Failed to fetch news. HTTP Status Code: {response.status_code}")
     
     return df
